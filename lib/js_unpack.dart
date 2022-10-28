b622c19d6afbe5e8ea0d4bd24ae7b733a31995b6
linkkader <59131542+linkkader@users.noreply.github.com>
2022-10-28 13:08:06 +0100

    first commit

---
+++ lib/js_unpack.dart
@@ -0,0 +1,39 @@
+/*
+
+    ResolveUrl Kodi Addon
+    Copyright (C) 2013 Bstrdsmkr
+    Additional fixes by mortael, jairoxyz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Adapted for use in xbmc from:
+    https://github.com/beautify-web/js-beautify/blob/master/python/jsbeautifier/unpackers/packer.py
+
+    Unpacker for Dean Edward's p.a.c.k.e.r, a part of javascript beautifier
+    by Einar Lielmanis <einar@jsbeautifier.org>
+
+        written by Stefano Sanfilippo <a.little.coder@gmail.com>
+
+    usage:
+
+    if detect(some_string):
+        unpacked = unpack(some_string)
+
+    Unpacker for Dean Edward's p.a.c.k.e.r
+
+* */
+
+library jsunpack;
+
+export 'src/jsunpack_base.dart';
